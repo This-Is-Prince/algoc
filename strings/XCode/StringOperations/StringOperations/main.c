09b2596a1c4f58b7b169c26b41e42bfe410f2d98
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-09-11 20:13:56 +0530

    feat: string operations

---
+++ strings/XCode/StringOperations/StringOperations/main.c
@@ -0,0 +1,259 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+struct String {
+    char *s;
+    int len;
+    int cap;
+};
+
+typedef struct String String;
+
+String* CreateString(int size) {
+    if (size < 0) {
+        perror("Size can't be negative ");
+        exit(EXIT_FAILURE);
+        return NULL;
+    }
+    
+    String *str = malloc(sizeof(String));
+    
+    str->s = calloc(size, sizeof(char));
+    str->len = 0;
+    str->cap = size;
+    
+    return str;
+}
+
+void Free(String *str) {
+    if (str == NULL) {
+        return;
+    }
+    
+    if (str->s != NULL) {
+        free(str->s);
+    }
+    
+    free(str);
+}
+
+void Display(String *str) {
+    if (str == NULL || str->s == NULL) {
+        return;
+    }
+
+    printf("Len: %d\n", str->len);
+    printf("Cap: %d\n", str->cap);
+    printf("%s\n", str->s);
+}
+
+void Append(String *str, const char *s) {
+    if (str == NULL) {
+        return;
+    }
+    
+    if (str->s == NULL) {
+        return;
+    }
+    
+    for (int i = 0; s[i] != '\0'; ++i) {
+        if (str->len >= str->cap) {
+            str->cap = str->cap == 0? 8: str->cap * 2;
+            str->s = realloc(str->s, str->cap * sizeof(char));
+        }
+        
+        str->s[str->len++] = s[i];
+    }
+}
+
+void LowerCase(String *str) {
+    if (str == NULL || str->s == NULL) {
+        return;
+    }
+
+    for (int i = 0; i < str->len; ++i) {
+        if (str->s[i] >= 65 && str->s[i] <= 90) {
+            str->s[i] = 97 + str->s[i] - 65;
+        }
+    }
+}
+
+void UpperCase(String *str) {
+    if (str == NULL || str->s == NULL) {
+        return;
+    }
+    
+    for (int i = 0; i < str->len; ++i) {
+        if (str->s[i] >= 97 && str->s[i] <= 122) {
+            str->s[i] = 65 + str->s[i] - 97;
+        }
+    }
+}
+
+bool IsVowel(char c) {
+    char vowels[] = "aeiouAEIOU";
+    
+    for (int i = 0; vowels[i] != '\0'; ++i) {
+        if (vowels[i] == c) {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+int VowelCount(String *str) {
+    if (str == NULL || str->s == NULL) {
+        return -1;
+    }
+    
+    int count = 0;
+    for (int i = 0; i < str->s[i]; ++i) {
+        if (IsVowel(str->s[i])) {
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+int WordCount(String *str) {
+    if (str == NULL || str->s == NULL) {
+        return -1;
+    }
+    
+    int count = 0;
+    int i;
+    
+    for (i = 0; i < str->s[i]; ++i) {
+        if (str->s[i] == ' ' && i > 0 && str->s[i - 1] != ' ') {
+            count++;
+        }
+    }
+    
+    if (i != 0 && str->s[i - 1] != ' ') {
+        count++;
+    }
+    
+    return count;
+}
+
+bool IsValidString(String *str) {
+    if (str == NULL || str->s == NULL) {
+        return false;
+    }
+    
+    for (int i = 0; i < str->s[i]; ++i) {
+        if (!((str->s[i] >= 48 && str->s[i] <= 57) || (str->s[i] >= 65 && str->s[i] <= 90) || (str->s[i] >= 97 && str->s[i] <= 122))) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+void Reverse(String *str) {
+    if (str == NULL || str->s == NULL) {
+        return;
+    }
+    
+    int first = 0, last = str->len - 1;
+    
+    while (first < last) {
+        char tmp = str->s[first];
+        str->s[first] = str->s[last];
+        str->s[last] = tmp;
+        first++;
+        last--;
+    }
+}
+
+bool IsEqual(String *str1, String *str2) {
+    if (str1 == NULL || str1->s == NULL) {
+        return false;
+    }
+    
+    if (str2 == NULL || str2->s == NULL) {
+        return false;
+    }
+    
+    int i = 0, j = 0;
+    
+    while (i < str1->len && j < str2->len) {
+        if (str1->s[i] != str2->s[j]) {
+            return false;
+        }
+        i++;
+        j++;
+    }
+    
+    if (!(i == str1->len && j == str2->len)) {
+        return false;
+    }
+    
+    return true;
+}
+
+bool IsPalindrome(String *str) {
+    if (str == NULL || str->s == NULL) {
+        return false;
+    }
+    
+    int i = 0, j = str->len - 1;
+    
+    while (i < j) {
+        if (str->s[i] != str->s[j]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+String* Duplicates(String *str) {
+    if (str == NULL || str->s == NULL) {
+        return NULL;
+    }
+    
+    String *r = CreateString(str->len);
+    
+    return r;
+}
+
+
+int main(int argc, const char* argv[]) {
+    String *str = CreateString(10);
+    Append(str, "   I     Love     Golang");
+    Display(str);
+    
+    LowerCase(str);
+    Display(str);
+    UpperCase(str);
+    Display(str);
+    printf("Vowel count is: %d\n", VowelCount(str));
+    printf("Word Count is: %d\n", WordCount(str));
+    Reverse(str);
+    Display(str);
+//    s = "321342adsgasdg4543";
+    
+    String *temp = CreateString(8);
+    Append(temp, "32145342#534dsagsgas");
+    Display(temp);
+    printf("Is string Valid: %d\n", IsValidString(temp));
+    
+    String *str1 = CreateString(10);
+    Append(str1, "Painter");
+    Display(str1);
+    String *str2 = CreateString(0);
+    Append(str2, "Painter");
+    Display(str2);
+    printf("Is string equal: %d\n", IsEqual(str1, str2));
+    
+    Free(str);
+    Free(temp);
+    Free(str1);
+    Free(str2);
+
+    return 0;
+}
